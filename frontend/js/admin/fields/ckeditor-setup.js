16dcffac906ab35603f6aca07e200dfa81da4292
mush42 <musharraf.omer@gmail.com>
2019-03-03 23:52:24 +0200

    Little changes to the styling of bs fields.

--- frontend/js/admin/fields/ckeditor-setup.js
+++ frontend/js/admin/fields/ckeditor-setup.js
@@ -1,9 +1,13 @@
 $(function() {
 
-  ClassicEditor
-    .create( document.querySelector('.ckeditor'))
-    .catch(function(error) {
-        console.error( error );
-  });
+  var allEditors = document.querySelectorAll("textarea.ckeditor")
+
+  for (var i = 0; i < allEditors.length; ++i) {
+    ClassicEditor
+      .create(allEditors[i])
+      .catch(function(error) {
+          console.error( error );
+    });
+  }
 
 });
